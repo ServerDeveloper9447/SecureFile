f766c05f044206e7247238618a9f82d02353dae9
ServerDeveloper9447 <soumalyaghosh2022@gmail.com>
2024-08-27 22:35:31 +0530

    Updated. New version

--- readme.md
+++ readme.md
@@ -9,9 +9,26 @@ const { SecureFile } = require('securefile')
 import { SecureFile } from 'securefile'
 
 // initialize
-const secure = new SecureFile('<your key here>')
+const secure = new SecureFile('<your secret key here>')
 
-secure.encrypt('<filePath>') // returns {fileName: string, filePath: string}
+secure.encrypt('<filePath>', encryptionOptions) // returns {fileName: string, filePath: string}
 
-secure.decrypt('<filePath>') // returns {fileName: string, filePath: string}
+secure.decrypt('<filePath>', overwrite?: boolean) // returns {fileName: string, filePath: string}
+```
+
+### Encryption Options
+| Options | Type | Default Value | Description |
+|---------|------|---------------|-------------|
+| `overwrite?` | `boolean` | `true` | Instead of creating a separate file, it overwrites the existing file. |
+| `rename?` | `boolean` | `true` | Renames the original file to a cryptic name. Ignored when `overwrite` is set to `false` |
+
+### Bulk Encryption and Decryption
+```js
+secure.encryptMany([{filePath: string, overwrite?: boolean, rename?: boolean},...])
+// or
+secure.encryptFolder('<folder_path'>) // encrypts each file inside a folder
+
+secure.decryptMany([{filePath: string, overwrite: boolean}],...)
+// or
+secure.decryptFolder('<folder_path>') // decrypts each file inside a folder
 ```
--- securefile.ts
+++ securefile.ts
@@ -1,82 +1,125 @@
-import crypto from 'crypto'
+import crypto from 'node:crypto'
 import fs from 'fs'
 import path from 'path'
 
+const ciphers = crypto.getCiphers();
+const hashes = crypto.getHashes();
 export class SecureFile {
-    #key: string;
-    /**
-     * 
-     * @param {String} key Key to encrypt
-     */
-    constructor(key:string) {
-        this.#key = key
+    #key: Buffer;
+    #algorithm: (typeof ciphers)[number];
+    
+    constructor(key:string, algorithm: string = 'aes-256-cbc', keyHash: (typeof hashes)[number] = 'sha256') {
+        this.#key = crypto.createHash(keyHash).update(key).digest()
+        this.#algorithm = algorithm
     }
 
     #encryptString(text: string, key: Buffer, iv: Buffer) {
-        const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+        const cipher = crypto.createCipheriv(this.#algorithm, key, iv);
         let encrypted = cipher.update(text, 'utf8', 'hex');
         encrypted += cipher.final('hex');
         return encrypted;
     }
 
-    /**
-     * 
-     * @param {String} filePath 
-     * @param {String|undefined} encryptKey 
-     * @returns 
-     */
-    encrypt(filePath: string, encryptKey: string | undefined) {
-        let k = encryptKey || this.#key
-        let key = crypto.createHash('sha256').update(k).digest()
+    encrypt(filePath: string, options: {
+        rename?: boolean,
+        overwrite?: boolean
+    } = {rename: true, overwrite: true}) {
+        let key = this.#key
         const iv = crypto.randomBytes(16);
-    const fileData = fs.readFileSync(filePath);
-    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-    let encryptedData = cipher.update(fileData);
-    encryptedData = Buffer.concat([encryptedData, cipher.final()]);
-    fs.writeFileSync(filePath, Buffer.concat([iv, encryptedData]));
-    const originalFileName = path.basename(filePath);
-    const encryptedFileName = this.#encryptString(originalFileName, key, iv);
-    const encryptedFilePath = path.join(path.dirname(filePath), encryptedFileName);
-    fs.renameSync(filePath, encryptedFilePath);
-    let rd: {fileName: string, filePath: string} = {fileName: encryptedFileName, filePath: encryptedFilePath}
+        const fileData = fs.readFileSync(filePath);
+        const cipher = crypto.createCipheriv(this.#algorithm, key, iv);
+        let encryptedData = cipher.update(fileData);
+        encryptedData = Buffer.concat([encryptedData, cipher.final()]);
+        const originalFileName = path.basename(filePath);
+        const encryptedFileName = this.#encryptString(originalFileName, key, iv);
+        const encryptedFilePath = path.join(path.dirname(filePath), encryptedFileName);
+        if (!options?.overwrite) {
+            fs.writeFileSync(encryptedFilePath, Buffer.concat([iv, encryptedData]));
+            let rd: {fileName: string, filePath: string} = {fileName: encryptedFileName, filePath: encryptedFilePath}
 
-    return rd;
+            return rd;
+        }
+        if (!options?.rename) {
+            fs.writeFileSync(filePath, Buffer.concat([iv, encryptedData]));
+            let rd: {fileName: string, filePath: string} = {fileName: originalFileName, filePath: filePath}
+
+            return rd;
+        }
+        fs.writeFileSync(filePath, Buffer.concat([iv, encryptedData]));
+        fs.renameSync(filePath, encryptedFilePath);
+        let rd: {fileName: string, filePath: string} = {fileName: encryptedFileName, filePath: encryptedFilePath}
+        return rd;
     }
 
     #decryptString(encryptedText: string, key: Buffer, iv: Buffer) {
-        const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+        const decipher = crypto.createDecipheriv(this.#algorithm, key, iv);
         let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
         decrypted += decipher.final('utf8');
         return decrypted;
     }
 
-    /**
-     * 
-     * @param {String} filePath 
-     * @param {String|undefined} decryptKey 
-     * @returns 
-     */
-    decrypt(filePath: string, decryptKey: string | undefined) {
-        let k = decryptKey || this.#key
-        let key = crypto.createHash('sha256').update(k).digest()
+    decrypt(filePath: string, overwrite: boolean = true) {
+        let key = this.#key
         const encryptedData = fs.readFileSync(filePath);
         if (encryptedData.length < 17) {
             throw new Error('The encrypted file is too short or corrupted.');
         }
         const iv = encryptedData.subarray(0, 16);
         const encryptedContent = encryptedData.subarray(16);
-        const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+        const decipher = crypto.createDecipheriv(this.#algorithm, key, iv);
         let decryptedData = Buffer.concat([
             decipher.update(encryptedContent),
             decipher.final(),
         ]);
-        fs.writeFileSync(filePath, decryptedData);
         const encryptedFileName = path.basename(filePath);
         const originalFileName = this.#decryptString(encryptedFileName, key, iv);
         const originalFilePath = path.join(path.dirname(filePath), originalFileName);
+        if(!overwrite) {
+            fs.writeFileSync(originalFilePath, decryptedData)
+            let rd: {fileName: string, filePath: string} = {fileName: originalFileName, filePath: originalFilePath}
+
+            return rd;
+        }
+        fs.writeFileSync(filePath, decryptedData);
         fs.renameSync(filePath, originalFilePath);
         let rd: {fileName: string, filePath: string} = {fileName: originalFileName, filePath: originalFilePath}
 
         return rd;
+    }
+
+    encryptMany(fileData: {filePath: string, overwrite?: boolean, rename?: boolean}[]) {
+        return fileData.map(file => this.encrypt(file.filePath, {overwrite: file?.overwrite, rename: file?.rename}))
+    }
+
+    decryptMany(fileData: {filePath: string, overwrite?: boolean}[]) {
+        return fileData.map(file => this.decrypt(file.filePath, file?.overwrite))
+    }
+
+    encryptFolder(folderPath: string) {
+        const files = fs.readdirSync(folderPath, { withFileTypes: true });
+        const paths = files.map(file => path.join(folderPath, file.name));
+
+        for (const filePath of paths) {
+            const stat = fs.statSync(filePath);
+            if (stat.isDirectory()) {
+                this.encryptFolder(filePath);
+            } else {
+                this.encrypt(filePath);
+            }
+        }
+    }
+
+    decryptFolder(folderPath: string) {
+        const files = fs.readdirSync(folderPath, { withFileTypes: true });
+        const paths = files.map(file => path.join(folderPath, file.name));
+
+        for (const filePath of paths) {
+            const stat = fs.statSync(filePath);
+            if (stat.isDirectory()) {
+                this.decryptFolder(filePath);
+            } else {
+                this.decrypt(filePath);
+            }  
+        }
     }
 }
